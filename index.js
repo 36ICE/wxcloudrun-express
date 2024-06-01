83d1f6e5df28b7d0641ecc40a2c3480fcb9e99e4
zengcd <635727914@qq.com>
2024-06-01 13:31:48 +0800

    修改

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(logger);
 // 首页
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-  console.log("接收到请求", req);
+  console.log("接收到请求1", req);
 
   return true;
 });
